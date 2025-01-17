d6cdbe03ac94ef0d57e31f6fbed2a1407788e6d8
oscarlinkticc <oscar.jimenez@linktic.com>
2025-01-17 15:05:08 -0500

    Pipeline

--- src/test/java/Linktic/pages/MainPages.java
+++ src/test/java/Linktic/pages/MainPages.java
@@ -1,7 +1,6 @@
 package Linktic.pages;
 
 import Linktic.utils.ConfigReader;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
